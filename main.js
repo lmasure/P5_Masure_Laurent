b759d646e4383c99dea10963c9ce83f82e0f786c
lmasure <laurent@masure.pro>
2020-07-29 09:14:04 +0200

    modif css

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const APIURL = "http://localhost:3000/api/teddies";
 const productsDOM = document.querySelector("#products-center");
 
-
+//connexion à l'API et affichage des objets dans la page index
 async function getTeddies() {
   let response = await fetch(APIURL);
   if(response.ok){
@@ -13,12 +13,12 @@ async function getTeddies() {
         <article class="product">
           <div class="img-container">
             <img src=${product.imageUrl} alt="product" class="product-img">
-            <button class="bag-btn" data-id=${product._id}>
-              <i class="fas fa-shopping-cart"></i>
-              ajouter au panier
+            <button class="product-btn" data-id=${product._id}>
+              <i class="fa fa-search"></i>
+              Détail
             </button>
             <h3>${product.name}</h3>
-          <h4>${product.price}€</h4>
+          <h4>${Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(product.price/100)}</h4>
           </div>
         </article>
         <!-- end of single product -->`;
